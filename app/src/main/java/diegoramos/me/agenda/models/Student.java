ead4ef5e4d21fa3e14961ec85367f5d0a61ffa22
ramos <rdiego26@gmail.com>
2018-02-04 19:17:30 -0200

    ADD more constructors and toString

--- app/src/main/java/diegoramos/me/agenda/models/Student.java
+++ app/src/main/java/diegoramos/me/agenda/models/Student.java
@@ -1,11 +1,14 @@
 package diegoramos.me.agenda.models;
 
 public class Student {
+    private int id;
     private String name;
     private String telephone;
     private String address;
     private String site;
     private double grade;
+
+    public Student() { }
 
     public Student(String name, String telephone, String address, String site, double grade) {
         this.name = name;
@@ -13,6 +16,29 @@ public class Student {
         this.address = address;
         this.site = site;
         this.grade = grade;
+    }
+
+    public Student(int id, String name, String telephone, String address, String site, double grade) {
+        this.id = id;
+        this.name = name;
+        this.telephone = telephone;
+        this.address = address;
+        this.site = site;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName() + " - " + this.getGrade();
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
