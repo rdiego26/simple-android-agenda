483e5091c62f051244f2ef68da3b8f50874a7601
ramos <rdiego26@gmail.com>
2018-02-04 19:12:49 -0200

    ADD methods to interact DB

--- app/src/main/java/diegoramos/me/agenda/dao/StudentDAO.java
+++ app/src/main/java/diegoramos/me/agenda/dao/StudentDAO.java
@@ -1,8 +1,15 @@
 package diegoramos.me.agenda.dao;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import diegoramos.me.agenda.models.Student;
 
 public class StudentDAO extends SQLiteOpenHelper {
 
@@ -26,5 +33,40 @@ public class StudentDAO extends SQLiteOpenHelper {
         String sql = "DROP TABLE IF EXISTS students;";
         db.execSQL(sql);
         onCreate(db);
+    }
+
+    public void insert(Student student) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", student.getName());
+        contentValues.put("telephone", student.getTelephone());
+        contentValues.put("address", student.getAddress());
+        contentValues.put("site", student.getSite());
+        contentValues.put("grade", student.getGrade());
+
+        db.insert("students", null, contentValues);
+
+        db.close();
+    }
+
+    public List<Student> getAllStudents() {
+        String sql = "SELECT * FROM students;";
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+
+        List<Student> students = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            Student student = new Student(
+                    cursor.getString(cursor.getColumnIndex("name")),
+                    cursor.getString(cursor.getColumnIndex("telephone")),
+                    cursor.getString(cursor.getColumnIndex("address")),
+                    cursor.getString(cursor.getColumnIndex("site")),
+                    cursor.getDouble(cursor.getColumnIndex("grade"))
+            );
+            students.add(student);
+        }
+        db.close();
+        return students;
     }
 }
