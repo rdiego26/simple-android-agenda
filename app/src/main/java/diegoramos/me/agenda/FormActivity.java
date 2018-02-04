9dd8a6da7939e123ecd167a5d1c5e6d08c8068aa
ramos <rdiego26@gmail.com>
2018-02-04 11:07:50 -0200

    ADD listener to button

--- app/src/main/java/diegoramos/me/agenda/FormActivity.java
+++ app/src/main/java/diegoramos/me/agenda/FormActivity.java
@@ -2,6 +2,9 @@ package diegoramos.me.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        Button btnAction = findViewById(R.id.form_student_button);
+        btnAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormActivity.this, "Clicked!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
