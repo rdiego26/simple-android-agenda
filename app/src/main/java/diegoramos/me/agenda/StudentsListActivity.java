e7a4dd8f5fa49cb75f00a6b7df5e9d1095629057
ramos <rdiego26@gmail.com>
2018-02-04 21:05:15 -0200

    IMPLEMENT delete

--- app/src/main/java/diegoramos/me/agenda/StudentsListActivity.java
+++ app/src/main/java/diegoramos/me/agenda/StudentsListActivity.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -16,27 +20,49 @@ import diegoramos.me.agenda.models.Student;
 
 public class StudentsListActivity extends AppCompatActivity {
 
+    private ListView studentsListVw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students_list);
-    }
 
-    private void getDataAndHandleToView() {
-        StudentDAO studentDAO = new StudentDAO(this);
-        List<Student> studentList = studentDAO.getAllStudents();
+        studentsListVw = findViewById(R.id.studentsList);
 
-        ListView studentsListVw = findViewById(R.id.studentsList);
-        ArrayAdapter<Student> adapter = new ArrayAdapter<Student>(this,
-                android.R.layout.simple_list_item_1, studentList);
-        studentsListVw.setAdapter(adapter);
-
+        registerForContextMenu(studentsListVw);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         getDataAndHandleToView();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuItem deleteMenuItem = menu.add("Deletar");
+
+        deleteMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                Student student = (Student) studentsListVw.getItemAtPosition(info.position);
+                StudentDAO studentDAO = new StudentDAO(StudentsListActivity.this);
+                studentDAO.delete(student);
+                return false;
+            }
+        });
+    }
+
+    private void getDataAndHandleToView() {
+        StudentDAO studentDAO = new StudentDAO(this);
+        List<Student> studentList = studentDAO.getAllStudents();
+
+        ArrayAdapter<Student> adapter = new ArrayAdapter<Student>(this,
+                android.R.layout.simple_list_item_1, studentList);
+        studentsListVw.setAdapter(adapter);
+
     }
 
     public void goToForm(View v) {
