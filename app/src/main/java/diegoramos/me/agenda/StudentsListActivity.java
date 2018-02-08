84a0e0c79d0612d5150168a4715adfe2040de560
ramos <rdiego26@gmail.com>
2018-02-08 00:37:24 -0200

    HANDLE with address .

--- app/src/main/java/diegoramos/me/agenda/StudentsListActivity.java
+++ app/src/main/java/diegoramos/me/agenda/StudentsListActivity.java
@@ -1,6 +1,5 @@
 package diegoramos.me.agenda;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -10,9 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -47,8 +44,13 @@ public class StudentsListActivity extends AppCompatActivity {
         final Student student = (Student) studentsListVw.getItemAtPosition(info.position);
 
         MenuItem visitSiteMenuItem = menu.add("Visitar Site");
-        listenVisitMenu(visitSiteMenuItem, student);
+        handleVisitSiteMenu(visitSiteMenuItem, student);
 
+        MenuItem sendSMSMenuItem = menu.add("Enviar SMS");
+        handleSendSMSMenu(sendSMSMenuItem, student);
+
+        MenuItem visitAddressMenuItem = menu.add("Ir para o endereço");
+        handleMapMenu(visitAddressMenuItem, student);
 
         MenuItem deleteMenuItem = menu.add("Deletar");
         deleteMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
@@ -63,16 +65,34 @@ public class StudentsListActivity extends AppCompatActivity {
 
     }
 
-    private void listenVisitMenu(MenuItem visitSiteMenuItem, Student student) {
-        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+    private void handleMapMenu(MenuItem menuItem, Student student) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String address = student.getAddress();
+        Uri uri = Uri.parse("geo:0,0?q=" + address);
+
+        intent.setData(uri);
+        menuItem.setIntent(intent);
+    }
+
+    private void handleSendSMSMenu(MenuItem menuItem, Student student) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String phone = student.getTelephone();
+        Uri uri = Uri.parse("sms:/" + phone);
+
+        intent.setData(uri);
+        menuItem.setIntent(intent);
+    }
+
+    private void handleVisitSiteMenu(MenuItem visitSiteMenuItem, Student student) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
         String site = student.getSite();
 
         if(!site.startsWith("http://")) {
             site = "http://" + site;
         }
 
-        intentSite.setData(Uri.parse(site));
-        visitSiteMenuItem.setIntent(intentSite);
+        intent.setData(Uri.parse(site));
+        visitSiteMenuItem.setIntent(intent);
     }
 
     private void getDataAndHandleToView() {
