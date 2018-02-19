0dd331b467ad00659db72c5c7beb8d011841794e
ramos <rdiego26@gmail.com>
2018-02-18 23:41:32 -0300

    HANDLE image logic

--- app/src/main/java/diegoramos/me/agenda/FormActivity.java
+++ app/src/main/java/diegoramos/me/agenda/FormActivity.java
@@ -1,12 +1,22 @@
 package diegoramos.me.agenda;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
 
 import diegoramos.me.agenda.dao.StudentDAO;
 import diegoramos.me.agenda.helpers.FormHelper;
@@ -14,7 +24,11 @@ import diegoramos.me.agenda.models.Student;
 
 public class FormActivity extends AppCompatActivity {
 
+    private static final int CAMERA_REQUEST_CODE = 567;
+
     private FormHelper formHelper;
+
+    private String photoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,7 @@ public class FormActivity extends AppCompatActivity {
 
         formHelper = new FormHelper(this);
         handleUpdateFlow();
+        handleCameraButton();
 
     }
 
@@ -51,6 +66,33 @@ public class FormActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == Activity.RESULT_OK && requestCode == CAMERA_REQUEST_CODE) {
+            ImageView imageView = findViewById(R.id.form_image);
+            Bitmap bitmap = BitmapFactory.decodeFile(photoPath);
+            Bitmap reducedBitmap = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+
+            imageView.setImageBitmap(reducedBitmap);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+    }
+
+    private void handleCameraButton() {
+        Button btnCamera = findViewById(R.id.form_btn_camera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                photoPath = getExternalFilesDir(null) + "/" + System.currentTimeMillis() +  ".jpg";
+                File photo = new File(photoPath);
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
     }
 
     private void handleUpdateFlow() {
